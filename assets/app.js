b70ce062d812461050ff164832a67360eb703cb9
Benjamin Wagner <benjamin.voigt@vapita.de>
2021-12-13 06:13:50 +0100

    javascript router updated

--- assets/app.js
+++ assets/app.js
@@ -2,10 +2,14 @@ import './styles/app.scss';
 import Core from "./app/core";
 
 let App = new Core();
+const xhttp = new XMLHttpRequest();
+xhttp.addEventListener("progress", updateProgress);
+xhttp.addEventListener("load", transferComplete);
+xhttp.addEventListener("error", transferFailed);
+xhttp.addEventListener("abort", transferCanceled);
 
 let submitButton = App.findOneBy('#list_delete');
 let formInputFields = App.findBy('.checkbox-action');
-let linkButtons = App.findBy('.link');
 
 App.setAttributes(App.findBy('.disabled'),'disabled','disabled')
 App.form.validate(App.findBy('.needs-validation'))
@@ -41,26 +45,78 @@ function validateDeleteForm($i){
         App.setAttribute(submitButton,'disabled','disabled')
     }
 }
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log("DOM fully loaded and parsed");
+    let linkButtons = App.findBy('a.link');
+    createLinkAction(linkButtons,loadPage,'data-link')
+});
 
-createLinkAction(linkButtons,loadPage,'data-link')
 
 function createLinkAction(elements,customFunction,attribute, eventAction = "click"){
     Array.prototype.slice.call(elements)
         .forEach(function (element) {
             let link = App.getAttribute(element,attribute);
-            element.addEventListener(eventAction,function() {
-                    loadPage(link);
+            element.addEventListener(eventAction,function(element) {
+                    element.preventDefault()
+                    element.stopPropagation()
+                setTimeout(function (){
+                    if(App.findOneBy('#loading-wrapper').classList.contains('fade-out'))
+                    {
+                        loadPage(link);
+                    }
+                },1000);
+                    App.setDisplay('#loading-wrapper','block')
+                    App.setClass(App.findOneBy('#loading-wrapper'),'fade-out');
                 },
                 false)
         })
 }
 
-function loadPage(link) {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function() {
-        App.findOneBy('#page').innerHTML = this.responseText;
+// progress on transfers from the server to the client (downloads)
+function updateProgress (oEvent) {
+    App.findOneBy('#loading').innerHTML = 'Ladevorgang gestartet';
+    App.setDisplay('#loading-wrapper','block');
+    console.log("The transfer started.");
+    if (oEvent.lengthComputable) {
+        let percentComplete = oEvent.loaded / oEvent.total * 100;
+        App.findOneBy('#loading').innerHTML = percentComplete + '%';
+    } else {
+        // Unable to compute progress information since the total size is unknown
+        App.findOneBy('#loading').innerHTML = 'unbekannte Größe';
     }
-    xhttp.open("GET", link);
+}
+
+function transferComplete(evt) {
+    App.setClass(App.findOneBy('#loading-wrapper'),'fade-out',true);
+    setTimeout(function (){
+        App.setClass(App.findOneBy('#loading-wrapper'),'fade-in',true);
+        App.setDisplay('#loading-wrapper','none');
+    },300)
+    App.setClass(App.findOneBy('#loading-wrapper'),'fade-in');
+    App.findOneBy('#loading').innerHTML = 'Laden abgeschlossen';
+    //App.setDisplay('#loading-wrapper','none');
+    console.log("The transfer is complete.");
+}
+
+function transferFailed(evt) {
+    console.log("An error occurred while transferring the file.");
+}
+
+function transferCanceled(evt) {
+    console.log("The transfer has been canceled by the user.");
+}
+
+function loadPage(link) {
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            let page = App.findOneBy('#page').innerHTML = this.responseText;
+            let dom = new DOMParser()
+            dom.parseFromString(page,'application/xhtml+xml');
+            history.pushState({ id: 0 }, "Seitentitel", link)
+            let newButtons = App.findBy('#page .link')
+            createLinkAction(newButtons,loadPage,'data-link')
+        }
+    }
+    xhttp.open("GET", link,true);
     xhttp.send();
-    return null
 }
--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -10,7 +10,21 @@ class AppController extends AbstractController
     public function index(): string
     {
         return $this->render('app/index.html.twig',[
-            'controller_name' => 'AppController'
+            'controller_name' => 'Home'
+        ]);
+    }
+
+    public function imprint(): string
+    {
+        return $this->render('app/imprint.html.twig',[
+            'controller_name' => 'Imprint'
+        ]);
+    }
+
+    public function tos(): string
+    {
+        return $this->render('app/tos.html.twig',[
+            'controller_name' => 'Terms of Service'
         ]);
     }
 
